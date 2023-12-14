9823a79ef7ebfc3c680777a882f500524b70ff45
wbouhdif <wbouhdif@hotmail.com>
2023-12-14 14:48:56 +0100

    added battery value/topic together with waypoint topic, which interacts with the turtlebot nav2 stack

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,6 @@ class MyApp extends StatelessWidget {
 class RoslibPSPad extends StatefulWidget {
   const RoslibPSPad({Key? key, required this.title}) : super(key: key);
 
-
   final String title;
 
   @override
@@ -31,12 +30,16 @@ class RoslibPSPad extends StatefulWidget {
 }
 
 class _RoslibPSPadState extends State<RoslibPSPad> {
-  String host = 'ws://192.168.2.5:9090';
+  String host = 'ws://192.168.0.100:9090';
   late Ros ros;
   late Topic cmdVelTopic;
   late Topic autonomousTopic;
+  late Topic batteryTopic;
+  late Topic waypointTopic;
+
   double turnAngularVelocity = 1.5;
   double forwardVelocity = 0.2;
+  double batteryPercentage = 0.0;
 
   bool isConnected = false;
   bool isAutonomousMode = false;
@@ -63,6 +66,79 @@ class _RoslibPSPadState extends State<RoslibPSPad> {
         reconnectOnClose: true,
         queueLength: 10,
         queueSize: 10);
+
+    batteryTopic = Topic(
+        ros: ros,
+        name: '/battery_state',
+        type: "sensor_msgs/BatteryState",
+        reconnectOnClose: true,
+        queueLength: 10,
+        queueSize: 10);
+
+
+    waypointTopic = Topic(
+        ros: ros,
+        name: '/goal_pose',
+        type: "geometry_msgs/msg/PoseStamped",
+        reconnectOnClose: true,
+        queueLength: 10,
+        queueSize: 10);
+
+    batteryTopic.subscribe(batteryCallback);
+    ros.connect();
+  }
+
+  Future<void> batteryCallback(Map<String, dynamic> message) async {
+    setState(() {
+      batteryPercentage = message['percentage'];
+    });
+  }
+
+
+  Widget buildBatteryIndicator(double batteryPercentage) {
+    Color batteryColor;
+    if (batteryPercentage > 75) {
+      batteryColor = Colors.green;
+    } else if (batteryPercentage > 25) {
+      batteryColor = Colors.yellow;
+    } else {
+      batteryColor = Colors.red;
+    }
+
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        Icon(Icons.battery_charging_full, color: batteryColor, size: 24),
+        SizedBox(width: 5),
+        Text(
+          '${batteryPercentage.toStringAsFixed(2)}%',
+          style: TextStyle(fontSize: 18),
+        )
+      ],
+    );
+  }
+
+  TextEditingController xController = TextEditingController();
+  TextEditingController yController = TextEditingController();
+
+  void sendWaypoint() {
+    var waypoint = {
+      'header': { 'frame_id': 'map' },
+      'pose': {
+        'position': {
+          'x': double.parse(xController.text),
+          'y': double.parse(yController.text),
+          'z': 0.0
+        },
+        'orientation': {
+          'x': 0.0,
+          'y': 0.0,
+          'z': 0.0,
+          'w': 1.0
+        }
+      }
+    };
+    waypointTopic.publish(waypoint);
   }
 
   void toggleAutonomousMode() {
@@ -72,7 +148,6 @@ class _RoslibPSPadState extends State<RoslibPSPad> {
       autonomousTopic.publish(autonomyMessage);
     });
   }
-
 
 
   int leftButtonState = 1;
@@ -86,17 +161,13 @@ class _RoslibPSPadState extends State<RoslibPSPad> {
     var linear = {'x': 0.0, 'y': 0.0, 'z': 0.0};
     var angular = {'x': 0.0, 'y': 0.0, 'z': 0.0};
     if (leftButtonState == -1) {
-      linear = {'x': 0.0, 'y': 0.0, 'z': 0.0};
       angular = {'x': 0.0, 'y': 0.0, 'z': turnAngularVelocity};
     } else if (rightButtonState == -1) {
-      linear = {'x': 0.0, 'y': 0.0, 'z': 0.0};
       angular = {'x': 0.0, 'y': 0.0, 'z': -turnAngularVelocity};
     } else if (forwardButtonState == -1) {
       linear = {'x': forwardVelocity, 'y': 0.0, 'z': 0.0};
-      angular = {'x': 0.0, 'y': 0.0, 'z': 0};
     } else if (backwardButtonState == -1) {
       linear = {'x': -forwardVelocity, 'y': 0.0, 'z': 0.0};
-      angular = {'x': 0.0, 'y': 0.0, 'z': 0};
     }
     var twist = {'linear': linear, 'angular': angular};
     if (lastDirectionTwist == twist &&
@@ -107,7 +178,6 @@ class _RoslibPSPadState extends State<RoslibPSPad> {
     cmdVelTopic.publish(twist);
     lastDirectionTwist = twist;
   }
-
 
   void leftCallback(int event) {
     leftButtonState = event;
@@ -129,21 +199,47 @@ class _RoslibPSPadState extends State<RoslibPSPad> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text('${widget.title}'),
+        actions: <Widget>[
+          buildBatteryIndicator(batteryPercentage),
+        ],
       ),
-      body: Center( // This centers its child vertically and horizontally within the Scaffold's available space.
-        child: DirectionPad(
-          diameter: 200, // This is your specified size for the DirectionPad.
-          leftCallback: leftCallback,
-          rightCallback: rightCallback,
-          forwardCallback: forwardCallback,
-          backwardCallback: backwardCallback,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            DirectionPad(
+              diameter: 200,
+              leftCallback: leftCallback,
+              rightCallback: rightCallback,
+              forwardCallback: forwardCallback,
+              backwardCallback: backwardCallback,
+            ),
+            TextField(
+              controller: xController,
+              decoration: InputDecoration(labelText: 'X Coordinate'),
+            ),
+            TextField(
+              controller: yController,
+              decoration: InputDecoration(labelText: 'Y Coordinate'),
+            ),
+            ElevatedButton(
+              onPressed: sendWaypoint,
+              child: Text(
+                'Send Waypoint',
+                style: TextStyle(fontSize: 18),
+              ),
+              style: ElevatedButton.styleFrom(
+                padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+              ),
+            ),
+          ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: toggleAutonomousMode, // link button press to the handler
+        onPressed: toggleAutonomousMode,
         tooltip: isAutonomousMode ? 'Switch to Manual' : 'Switch to Autonomous',
-        child: Icon(isAutonomousMode ? Icons.autorenew : Icons.directions_car), // pre-defined icons
+        child: Icon(isAutonomousMode ? Icons.autorenew : Icons.directions_car),
       ),
     );
   }
